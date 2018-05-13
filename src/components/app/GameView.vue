898408c208af46a1ce3110bed4693941c1a585c7
Dominik <domi38h@gmail.com>
2018-05-13 14:51:21 +0200

    bugfixing

--- src/components/app/GameView.vue
+++ src/components/app/GameView.vue
@@ -1,39 +1,41 @@
 <template>
     <main id="game" class="wrapper">
         <ul class="card-stack">
-            <transition name="card" mode="out-in">
-                <li class="card" v-if="showCard">
-                    <div class="card__front start" v-if="card.type == 'start'">
-                        <img src="../../../static/img/ui/logo.svg" alt="Logo" class="card__logo--start">
-                    </div>
-                    <div class="card__front" v-else>
-                        <div class="card__img" :style="{ backgroundImage: 'url(' + cardImg + ')' }">
-                            <div class="card__gradient"></div>
+            <div class="perspective">
+                <transition name="card" mode="out-in">
+                    <li class="card" v-if="showCard">
+                        <div class="card__front start" v-if="card.type == 'start'">
+                            <img src="../../../static/img/ui/logo.svg" alt="Logo" class="card__logo--start">
                         </div>
-                        <div class="card__content">
-                            <div class="card__badge positiv" v-if="card.type == 'positiv'">
-                                <span></span>
-                                <p class="card__type normal">positiv</p>
+                        <div class="card__front" v-else>
+                            <div class="card__img" :style="{ backgroundImage: 'url(' + cardImg + ')' }">
+                                <div class="card__gradient"></div>
                             </div>
-                            <div class="card__badge neutral" v-if="card.type == 'neutral'">
-                                <span></span>
-                                <p class="card__type normal">neutral</p>
-                            </div>
-                            <div class="card__badge danger" v-if="card.type == 'negativ'">
-                                <span></span>
-                                <p class="card__type normal">negativ</p>
-                            </div>
-                            <div class="card__text">
-                                <p class="card__desc italic small">{{ card.desc }}</p>
-                                <p class="card__instruction normal">{{ card.instruction }}</p>
+                            <div class="card__content">
+                                <div class="card__badge positiv" v-if="card.type == 'positiv'">
+                                    <span></span>
+                                    <p class="card__type normal">positiv</p>
+                                </div>
+                                <div class="card__badge neutral" v-if="card.type == 'neutral'">
+                                    <span></span>
+                                    <p class="card__type normal">neutral</p>
+                                </div>
+                                <div class="card__badge danger" v-if="card.type == 'negativ'">
+                                    <span></span>
+                                    <p class="card__type normal">negativ</p>
+                                </div>
+                                <div class="card__text">
+                                    <p class="card__desc italic small">{{ card.desc }}</p>
+                                    <p class="card__instruction normal">{{ card.instruction }}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card__back">
-                        <img src="../../../static/img/ui/logo-mobile.svg" alt="Logo" class="card__logo">
-                    </div>
-                </li>
-            </transition>
+                        <div class="card__back">
+                            <img src="../../../static/img/ui/logo-mobile.svg" alt="Logo" class="card__logo">
+                        </div>
+                    </li>
+                </transition>
+            </div>
             <li class="card card--bg">
                 <div class="card__back">
                     <img src="../../../static/img/ui/logo-mobile.svg" alt="Logo" class="card__logo">
