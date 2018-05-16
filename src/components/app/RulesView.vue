b0e84ae81f8eddb71d51ac610be45edf290e80c3
Dominik <domi38h@gmail.com>
2018-05-16 19:51:41 +0200

    content

--- src/components/app/RulesView.vue
+++ src/components/app/RulesView.vue
@@ -2,7 +2,8 @@
     <main id="rules">
         <div class="wrapper wrapper--small">
             <h1 class="site-title">Regeln</h1>
-            <p>Wie jedes Spiel hat auch Papergolf bestimmte Regeln. Doch keine Angst, es sind nicht wirklich viele. Wenn du das Spiel noch nicht kennst, kannst du dir gerne die Videoanleitung unten anschauen. Dort erfährst du in kompakter Form, wie das Spiel funktioniert und was du beachten musst.</p>
+            <p>Das Ziel beim Papergolf ist das selbe wie im richtigen Golf. Es geht darum, den Ball im Loch zu versenken.</p>
+            <p>Bevor du allerdings beginnst die Spielanleitung zu lesen, überlege dir, ob du nicht lieber das Erklärungsvideo anschauen möchtest.</p>
             <h2>Videoanleitung</h2>
         </div>
         <div class="wrapper">
@@ -12,12 +13,23 @@
         </div>
         <div class="wrapper wrapper--small">
             <h2>Und hier noch schriftlich...</h2>
-            <p>Wenn du dir Anleitungen lieber durchliest, haben wir hier für dich alle Regeln auch noch in Textform zusammengefasst:</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque atque similique ratione nostrum consequatur amet, necessitatibus voluptatum ut dolorum doloremque asperiores esse commodi, neque porro natus inventore magnam mollitia numquam.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque atque similique ratione nostrum consequatur amet, necessitatibus voluptatum ut dolorum doloremque asperiores esse commodi, neque porro natus inventore magnam mollitia numquam.</p>
-            <h3>Random Subtitle</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque atque similique ratione nostrum consequatur amet, necessitatibus voluptatum ut dolorum doloremque asperiores esse commodi, neque porro natus inventore magnam mollitia numquam.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque atque similique ratione nostrum consequatur amet, necessitatibus voluptatum ut dolorum doloremque asperiores esse commodi, neque porro natus inventore magnam mollitia numquam.</p>
+            <p>Nun du hast dich also doch noch fürs Lesen entschieden :)</p>
+            <p>Um Papergolf zu Spielen braucht es nicht mehr als ein Stift und ein Blatt Papier.
+            Zuerst müssen die Spielbahnen gezeichnet oder ausgedruckt werden. Beispiele dazu findest du auf unserer Website. Ebenfalls auf unserer Webseite findest du viele verschiedene Aktionskarten, welche das Spiel spannender machen.</p>
+            <h3>Los geht’s</h3>
+            <ol>
+                <li>Der erste Spieler zieht eine Aktionskarte.</li>
+                <li>Nun wird der Stift beim Start/Abschlag angesetzt und die Augen geschlossen.</li>
+                <li>Ziel ist es nun einen Strich vom Abschlag zum Ziel/Loch zu ziehen.</li>
+                <li>Wenn der Ball/Strich über die Golfbahn hinaus geht, dann sagen die Mitspieler sofort Stopp. Beim nächsten Zug muss wieder eine Aktionskarte gezogen werden und von dort weiter gespielt werden, wo der Ball/Strich die Golfbahn verlassen hat.</li>
+                <li>Wenn der Ball/Strich das Loch erreicht hat, wird die Anzahl Schläge notiert und der nächste Spieler ist an der Reihe.</li>
+            </ol>
+            <h3>Zusatzregeln</h3>
+            <p>Bei den Golfbahnen auf Papergolf gibt es zusätzlich Wasserhindernisse (Wellensymbole) und Bunker/Sandhindernisse (gestrichelt). Wie auf auf einem normalen Golfplatz ist es auch beim Papergolf nicht von Vorteil diese Hindernisse zu Treffen.</p>
+            <ol>
+                <li><span class="bold positiv">Wasserhindernisse: </span>Wenn man ein Wasserhindernis trifft, kann man einfach weiterspielen. Jedoch erhält man am Schluss des Schlages einen Strafpunkt.</li>
+                <li><span class="bold positiv">Sandhindernisse («Bunker»): </span>Landet der Ball/Strich im Bunker, so sagen die Mitspieler ebenfalls Stopp. Anschliessend wird keine Aktionskarte gezogen, sondern mit der linken Hand weitergespielt.</li>
+            </ol>
             <p class="h4 bold positiv">Und jetzt viel Spass beim Spielen!</p>
         </div>
     </main>
